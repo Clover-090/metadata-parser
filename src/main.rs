6dbbdcbb73c7c034bb9084c327012c936547a3f0
Clover-090 <Clover.c21@proton.me>
2025-03-09 01:24:26 -0500

    using id3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use std::fs;
+use id3::Tag;
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Enter path to music file");
+
+    let mut pathtofile= String::new();
+
+    io::stdin()
+        .read_line(&mut pathtofile)
+        .expect("Failed to read line");
+
+    let tag = Tag::read_from_path(pathtofile).unwrap();
+
+    if let Some(artist) =  tag.artist() {
+        println!("artist: {}", artist);
+    }
 }
