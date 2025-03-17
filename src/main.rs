89ae5122f89be70bbf873433b1604a8bad982615
Clover-090 <Clover.c21@proton.me>
2025-03-16 22:09:31 -0400

    basic ability to read artist tag

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
-use id3::Tag;
+extern crate id3;
 use std::io;
+use id3::{Tag, TagLike};
 
 fn main() {
     println!("Enter path to music file");
 
-    let mut pathtofile= String::new();
+    let mut pathtofile = String::new();
 
     io::stdin()
         .read_line(&mut pathtofile)
         .expect("Failed to read line");
 
-    let tag = Tag::read_from_path(pathtofile).unwrap();
+        if pathtofile.ends_with('\n') {
+            pathtofile.pop();
+        }
+        if pathtofile.ends_with('\r') {
+            pathtofile.pop(); 
+        }
 
-    if let Some(artist) =  tag.artist() {
+    let _testpath = String::from("/home/clover/Desktop/music/Dirty Cheap Horse/On The Rob/Cheap Dirty Horse - On the Rob.mp3");
+
+    println!("{}", pathtofile);
+
+    let tag = Tag::read_from_path(pathtofile).expect("Failed to read tag");
+    
+    if let Some(artist) = tag.artist() {
         println!("artist: {}", artist);
-    }
+}
 }
